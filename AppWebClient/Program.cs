0a82aa8ceec177714160ce416a8162223db27677
chengliang_zheng <chengliang_zheng@foxmail.com>
2018-09-02 18:17:29 +0800

    ClientFactory依赖注入方式Demo

--- AppWebClient/Program.cs
+++ AppWebClient/Program.cs
@@ -1,5 +1,13 @@
-﻿using Microsoft.AspNetCore;
+﻿using System;
+using System.Threading.Tasks;
+using Demo.IGrain;
+using Lib;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Orleans;
+using Orleans.Runtime;
 
 namespace AppWebClient
 {
@@ -7,6 +15,7 @@ namespace AppWebClient
     {
         public static void Main(string[] args)
         {
+            StartClientWithRetries().GetAwaiter().GetResult();
             BuildWebHost(args).Run();
         }
 
@@ -14,5 +23,38 @@ namespace AppWebClient
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .Build();
+        private static async Task<IClusterClient> StartClientWithRetries(int initializeAttemptsBeforeFailing = 5)
+        {
+            int attempt = 0;
+            IClusterClient client;
+            while (true)
+            {
+                try
+                {
+                    client = await ClientFactory.Build(() =>
+                    {
+                        var builder = new ClientBuilder()
+                            .UseLocalhostClustering()
+                            .ConfigureApplicationParts(parts => parts.AddApplicationPart(typeof(IHello).Assembly).WithReferences())
+                            .ConfigureLogging(logging => logging.AddConsole());
+                        return builder;
+                    });
+                    Console.WriteLine("Client successfully connect to silo host");
+                    break;
+                }
+                catch (SiloUnavailableException)
+                {
+                    attempt++;
+                    Console.WriteLine($"Attempt {attempt} of {initializeAttemptsBeforeFailing} failed to initialize the Orleans client.");
+                    if (attempt > initializeAttemptsBeforeFailing)
+                    {
+                        throw;
+                    }
+                    await Task.Delay(TimeSpan.FromSeconds(4));
+                }
+            }
+
+            return client;
+        }
     }
 }
--- AppWebClient/Startup.cs
+++ AppWebClient/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Lib;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -18,6 +19,7 @@ namespace AppWebClient
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddSingleton<IClientFactory, ClientFactory>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
